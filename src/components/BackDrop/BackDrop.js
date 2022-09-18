1345a5b08e68539722d40a74d935e55c845e65c8
17eugene <drivercoua@gmail.com>
2022-09-19 02:10:43 +0300

    add zIndex to backdrop

--- src/components/BackDrop/BackDrop.js
+++ src/components/BackDrop/BackDrop.js
@@ -1,7 +1,12 @@
 import { Backdrop } from "@mui/material";
+import { styled } from "@mui/material";
+
+const StyledBackdrop = styled(Backdrop)(() => ({
+  zIndex: 2000,
+}));
 
 const BackDrop = ({ children, open }) => {
-  return <Backdrop open={open}>{children}</Backdrop>;
+  return <StyledBackdrop open={open}>{children}</StyledBackdrop>;
 };
 
 export default BackDrop;
